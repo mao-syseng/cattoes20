9b248e280ca9ba47c9d1101e079571cb1f8c590a
Marc Obel <mao@syseng.dk>
2025-01-31 13:34:41 +0100

    a

---
+++ kmk/code.py
@@ -0,0 +1,45 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+from kmk.modules.combos import Combos, Chord, Sequence
+
+keyboard = KMKKeyboard()
+combos = Combos()
+keyboard.modules.append(combos)
+
+keyboard.col_pins = (board.GP0, board.GP3, board.GP5, board.GP6)
+keyboard.row_pins = (board.GP15, board.GP18, board.GP22)
+keyboard.diode_orientation = DiodeOrientation.ROW2COL
+
+keyboard.keymap = [
+    [KC.I, KC.N, KC.S, KC.R,
+     KC.E, KC.T, KC.O, KC.A,
+     KC.BSPC, KC.SPC, KC.NO, KC.NO]
+]
+
+combos.combos = [
+    Chord((KC.R, KC.S), KC.B),
+    Chord((KC.N, KC.I), KC.Y),
+    Chord((KC.T, KC.E), KC.H),
+    Chord((KC.A, KC.O), KC.L),
+    Chord((KC.S, KC.N), KC.P),
+    Chord((KC.O, KC.T), KC.U),
+    Chord((KC.R, KC.I), KC.G),
+    Chord((KC.A, KC.E), KC.D),
+    Chord((KC.R, KC.N), KC.Z),
+    Chord((KC.S, KC.I), KC.F),
+    Chord((KC.A, KC.T), KC.Q),
+    Chord((KC.O, KC.E), KC.C),
+    Chord((KC.R, KC.T), KC.X),
+    Chord((KC.O, KC.I), KC.K),
+    Chord((KC.S, KC.E), KC.V),
+    Chord((KC.N, KC.A), KC.J),
+    Chord((KC.R, KC.E), KC.M),
+    Chord((KC.A, KC.I), KC.W),
+]
+
+if __name__ == '__main__':
+    keyboard.go()
+
